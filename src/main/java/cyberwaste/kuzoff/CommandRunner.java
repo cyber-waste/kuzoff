43c2f366b62183fdf4e2e6066e0a40b0adb8b2e3
yermilov <yermilov17@gmail.com>
2013-09-09 23:47:08 +0300

    add SimpleClient command line interface

---
+++ src/main/java/cyberwaste/kuzoff/CommandRunner.java
@@ -0,0 +1,14 @@
+package cyberwaste.kuzoff;
+
+import java.util.Map;
+
+public class CommandRunner {
+
+    public CommandRunner(String databaseFolder) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void runCommand(String commandName, Map<String, String> parameters) {
+        throw new UnsupportedOperationException();
+    }
+}
