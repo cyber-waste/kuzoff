1d70278aacb4f581e594f53054fd3d5001dbb28f
VladGritsun <vlg92@mail.ru>
2013-09-09 23:33:49 +0300

    database skeleton

---
+++ src/main/java/cyberwaste/kuzoff/domain/Table.java
@@ -0,0 +1,16 @@
+package cyberwaste.kuzoff.domain;
+
+import java.util.Collection;;
+
+public class Table {
+	
+	public Table(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	private String name;
+}
