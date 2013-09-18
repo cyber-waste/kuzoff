124dae0f3716332fad066f0b71b412cccb38fd06
VladGritsun <vlg92@mail.ru>
2013-09-18 17:30:57 +0300

    few simple tests added

--- src/main/java/cyberwaste/kuzoff/test/DatabaseManagerTest.java
+++ src/main/java/cyberwaste/kuzoff/test/DatabaseManagerTest.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import junit.framework.TestCase;
 
@@ -18,8 +20,10 @@ import org.junit.Test;
 
 
 import cyberwaste.kuzoff.database.DatabaseManager;
+import cyberwaste.kuzoff.database.Row;
 import cyberwaste.kuzoff.database.Table;
 import cyberwaste.kuzoff.database.Type;
+import cyberwaste.kuzoff.database.Value;
 
 public class DatabaseManagerTest {
     
@@ -31,6 +35,13 @@ public class DatabaseManagerTest {
         List<String> types = new ArrayList<String>();
         types.add("int"); types.add("char");
         manager.createTable("table1",types);
+        List<String> row = new ArrayList<String>();
+        row.add("10"); row.add("a");
+        manager.addRow("table1",row);
+        row = new ArrayList<String>();
+        row.add("20"); row.add("b");
+        manager.addRow("table1", row);
+        
     }
 
     @After
@@ -39,7 +50,7 @@ public class DatabaseManagerTest {
     }
 
     @Test
-    public void test() throws IOException {
+    public void listTablesTest() throws IOException {
         String[] typeNames = {"int", "char"}; 
         Collection<Table> tables = manager.listTables();
         assertEquals(1, tables.size());
@@ -49,6 +60,34 @@ public class DatabaseManagerTest {
         List<Type> types = table.columnTypes();
         for(int i=0;i<types.size();i++){
             assertEquals(types.get(i).name(),typeNames[i]);
+        }
+    }
+    
+    @Test
+    public void loadTableDataTest() throws IOException{
+        List<Row> rows = manager.loadTableData("table1");
+        assertEquals(2,rows.size());
+        for(Row row : rows) assertEquals(2,row.length());
+        String[][] rowDataActual = {{"10","a"},{"20","b"}};
+        for(int i=0; i<rows.size();i++){
+            Row row = rows.get(i);
+            for(int j=0;j<row.length();j++){
+                assertEquals(row.getElement(j).getValue(), rowDataActual[i][j]);
+            }
+        }
+    }
+    
+    @Test
+    public void removeRowTest() throws IOException{
+        Map<Integer,String> columnData = new HashMap<Integer, String>();
+        columnData.put(2, "a");
+        manager.removeRow("table1", columnData);
+        List<Row> rows = manager.loadTableData("table1");
+        assertEquals(1,rows.size());
+        Row row = rows.get(0);
+        String[] rowDataActual = {"20","b"};
+        for(int i=0;i<row.length();i++){
+            assertEquals(row.getElement(i).getValue(), rowDataActual[i]);
         }
     }
 
