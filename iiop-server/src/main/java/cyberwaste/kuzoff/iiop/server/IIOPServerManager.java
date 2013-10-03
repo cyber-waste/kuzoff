0987171b4d53815dac37697eb3bd286f36afd180
yermilov <yermilov17@gmail.com>
2013-10-04 00:42:09 +0300

    Fix errors in iiop-server subproject

--- iiop-server/src/main/java/cyberwaste/kuzoff/iiop/server/IIOPServerManager.java
+++ iiop-server/src/main/java/cyberwaste/kuzoff/iiop/server/IIOPServerManager.java
@@ -14,45 +14,48 @@ import org.omg.PortableServer.POA;
 import org.omg.PortableServer.RequestProcessingPolicyValue;
 import org.omg.PortableServer.Servant;
 import org.omg.PortableServer.ServantRetentionPolicyValue;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class IIOPServerManager {
     
-    public IIOPServerManager(String[] args) {
-        try {
-            Properties p = System.getProperties();
-            p.put("org.omg.CORBA.ORBClass", "com.sun.corba.se.internal.POA.POAORB");
-            p.put("org.omg.CORBA.ORBSingletonClass", "com.sun.corba.se.internal.corba.ORBSingleton");
-            
-            ORB orb = ORB.init( args, p );
-            
-            POA rootPOA = (POA)orb.resolve_initial_references("RootPOA");
-            
-            Policy[] tpolicy = new Policy[3];
-            tpolicy[0] = rootPOA.create_lifespan_policy(LifespanPolicyValue.TRANSIENT );
-            tpolicy[1] = rootPOA.create_request_processing_policy(RequestProcessingPolicyValue.USE_ACTIVE_OBJECT_MAP_ONLY );
-            tpolicy[2] = rootPOA.create_servant_retention_policy(ServantRetentionPolicyValue.RETAIN);
-            POA tPOA = rootPOA.create_POA("MyTransientPOA", null, tpolicy);
-            
-            tPOA.the_POAManager().activate();
-            
-            RemoteDatabaseManagerImpl impl = new RemoteDatabaseManagerImpl();
-            Tie tie = (Tie)Util.getTie( impl );
-            String dbId = "db";
-            byte[] id = dbId.getBytes();
-            tPOA.activate_object_with_id( id, (Servant) tie );
-            
-            Context initialNamingContext = new InitialContext();
-            initialNamingContext.rebind("DatabaseService", tPOA.create_reference_with_id(id,((Servant) tie)._all_interfaces(tPOA,id)[0]) );
-            System.out.println("Database Server: Ready...");
-            
-            orb.run();
-        } catch (Exception e) {
-            System.out.println("Problem running Database Server: " + e);
-            e.printStackTrace();
-        }
+    private RemoteDatabaseManagerImpl remoteDatabaseManager;
+    
+    @SuppressWarnings("resource")
+    public static void main(String[] args) throws BeansException, Exception {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("iiopServerContext.xml");
+        applicationContext.getBean(IIOPServerManager.class).start(args);
     }
     
-    public static void main(String args[]) {
-        new IIOPServerManager( args );
+    public void start(String[] args) throws Exception {
+        Properties p = System.getProperties();
+        p.put("org.omg.CORBA.ORBClass", "com.sun.corba.se.internal.POA.POAORB");
+        p.put("org.omg.CORBA.ORBSingletonClass", "com.sun.corba.se.internal.corba.ORBSingleton");
+        
+        ORB orb = ORB.init(args, p);
+        
+        POA rootPOA = (POA) orb.resolve_initial_references("RootPOA");
+        
+        Policy[] tpolicy = new Policy[3];
+        tpolicy[0] = rootPOA.create_lifespan_policy(LifespanPolicyValue.TRANSIENT);
+        tpolicy[1] = rootPOA.create_request_processing_policy(RequestProcessingPolicyValue.USE_ACTIVE_OBJECT_MAP_ONLY);
+        tpolicy[2] = rootPOA.create_servant_retention_policy(ServantRetentionPolicyValue.RETAIN);
+        POA tPOA = rootPOA.create_POA("MyTransientPOA", null, tpolicy);
+        
+        tPOA.the_POAManager().activate();
+        Tie tie = (Tie) Util.getTie(remoteDatabaseManager);
+        byte[] id = "db".getBytes();
+        tPOA.activate_object_with_id(id, (Servant) tie);
+            
+        Context initialNamingContext = new InitialContext();
+        initialNamingContext.rebind("DatabaseService", tPOA.create_reference_with_id(id,((Servant) tie)._all_interfaces(tPOA,id)[0]) );
+        System.out.println("Database Server: Ready...");
+        
+        orb.run();
+    }
+    
+    public void setRemoteDatabaseManager(RemoteDatabaseManagerImpl remoteDatabaseManager) {
+        this.remoteDatabaseManager = remoteDatabaseManager;
     }
 }
